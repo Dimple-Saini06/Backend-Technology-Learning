9fc72918c80e3011c5d1311efa54d21aa2fa45a8
dimple <dimple13619saini@gmail.com>
2026-06-25 23:43:48 +0530

    Day-2

--- script.js
+++ script.js
@@ -2,6 +2,12 @@
 
 // console.log(n);
 // console.log("bye");
+// console.log(process.argv)
 
 
-console.log(process.argv)
+// let data = require('./math');
+// console.log(data.sum(4,4));
+
+
+const info = require('./Fruits')
+console.log(info)
