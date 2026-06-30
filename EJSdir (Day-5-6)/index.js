10b28237af67384547facb17e8f5c8dfaf5018df
dimple <dimple13619saini@gmail.com>
2026-06-30 23:34:56 +0530

    Day-6

--- EJSdir (Day-5-6)/index.js
+++ EJSdir (Day-5-6)/index.js
@@ -3,6 +3,11 @@ const app = express();
 
 const port = 8080;
 
+app.set("view engine", "ejs");
+
+app.get("/", (req,res)=>{
+    res.render('home.ejs')
+})
 app.listen(port, ()=>{
     console.log(`listening on ${port}.`)
 })
